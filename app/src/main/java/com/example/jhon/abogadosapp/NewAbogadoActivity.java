aff18b0cac3216457a47a02d5e0b30e1823bb16e
Jhon <jftrujillo@unicauca.edu.co>
2016-10-14 08:42:10 -0500

    interfaz guardar valores

---
+++ app/src/main/java/com/example/jhon/abogadosapp/NewAbogadoActivity.java
@@ -0,0 +1,13 @@
+package com.example.jhon.abogadosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAbogadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_abogado);
+    }
+}
