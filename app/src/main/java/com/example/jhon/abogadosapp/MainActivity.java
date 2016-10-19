931b012a109133ed7d9f4e50a334584cf787bbda
Jhon <jftrujillo@unicauca.edu.co>
2016-10-19 07:24:19 -0500

    Preparacion para cambio a backend con green dao

--- app/src/main/java/com/example/jhon/abogadosapp/MainActivity.java
+++ app/src/main/java/com/example/jhon/abogadosapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.jhon.abogadosapp;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.jhon.abogadosapp.adapters.ListAbogadosAdapter;
@@ -14,19 +17,21 @@ import com.example.jhon.abogadosapp.models.AbogadoContract;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     AbogadoDbHelper helper;
     Cursor cursor;
     ListView list;
     ListAbogadosAdapter adapter;
     List<Abogado> data;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         list = (ListView) findViewById(R.id.list);
-
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
         helper = new AbogadoDbHelper(this);
         cursor = helper.getAllLawyer();
         data = new ArrayList<>();
@@ -56,5 +61,42 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter = new ListAbogadosAdapter(data,this);
         list.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Cursor cursor = helper.getAllLawyer();
+        data.clear();
+        while(cursor.moveToNext()){
+            //region Consulta de abogados en base de datos
+            Abogado abogado = new Abogado();
+            String nombre = cursor.getString(
+                    cursor.getColumnIndex(
+                            AbogadoContract.LawyerEntry.NAME));
+            abogado.setNombre(nombre);
+            String cargo = cursor.getString(
+                    cursor.getColumnIndex(AbogadoContract.LawyerEntry.CARGO)
+            );
+            abogado.setCargo(cargo);
+
+            String id = cursor.getString(
+                    cursor.getColumnIndex(AbogadoContract.LawyerEntry.ID)
+            );
+            abogado.setId(id);
+
+            String descripcion = cursor.getString(
+                    cursor.getColumnIndex(AbogadoContract.LawyerEntry.DESCRIPCION)
+            );
+            abogado.setDescripcion(descripcion);
+            data.add(abogado);
+            //endregion
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this,NewAbogadoActivity.class));
     }
 }
--- app/src/main/java/com/example/jhon/abogadosapp/NewAbogadoActivity.java
+++ app/src/main/java/com/example/jhon/abogadosapp/NewAbogadoActivity.java
@@ -1,13 +1,39 @@
 package com.example.jhon.abogadosapp;
 
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewAbogadoActivity extends AppCompatActivity {
+import com.example.jhon.abogadosapp.dataBase.AbogadoDbHelper;
+import com.example.jhon.abogadosapp.models.Abogado;
 
+public class NewAbogadoActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText nombre, cedula, cargo, descripcion;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_abogado);
+        nombre = (EditText) findViewById(R.id.nombre);
+        cedula = (EditText) findViewById(R.id.cedula);
+        cargo  = (EditText) findViewById(R.id.cargo);
+        descripcion = (EditText) findViewById(R.id.descripcion);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Abogado abogado = new Abogado();
+        abogado.setNombre(nombre.getText().toString());
+        abogado.setDescripcion(descripcion.getText().toString());
+        abogado.setCargo(cargo.getText().toString());
+        abogado.setId(cedula.getText().toString());
+        AbogadoDbHelper helper = new AbogadoDbHelper(this);
+        helper.guardarAbogado(abogado);
+        finish();
     }
 }
